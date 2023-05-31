9bce95a6fe01ea1c9858b5c19bf236821f72ca5f
Tony Alhwayek <60246873+TAlhwayek@users.noreply.github.com>
2023-05-30 19:40:29 -0700

    First commit

--- Bank It/ViewController.swift
+++ Bank It/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // Help button IBOutlet
+    @IBOutlet weak var helpButton: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Help button section
+        helpButton.isUserInteractionEnabled = true
+        
+        // Create a tap gesture recognizer
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(imageTapped))
+        
+        // Add the tap gesture recognizer to the image view
+        helpButton.addGestureRecognizer(tapGesture)
     }
-
-
+    
+    // Start button pressed
+    @IBAction func startButton(_ sender: Any) {
+        // Open main game
+        func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            let destinationVC = segue.destination as UIViewController
+            destinationVC.title = "gameView"
+        }
+    }
+    
+    @objc func imageTapped() {
+        performSegue(withIdentifier: "helpMenu", sender: self)
+    }
 }
 
---
+++ Bank It/gameViewController.swift
@@ -0,0 +1,56 @@
+//
+//  gameViewController.swift
+//  Bank It
+//
+//  Created by Tony Alhwayek on 5/30/23.
+//
+
+import UIKit
+
+class gameViewController: UIViewController {
+    // Bank object
+    var newGame: BankClass?
+    
+    // Labels
+    @IBOutlet weak var scoreLabel: UILabel!
+    // Test
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        // Create new object
+        newGame = BankClass()
+        // Update UI
+        updateUI()
+        // Load view
+        super.viewDidLoad()
+        
+
+    }
+    
+    // Updates score and number labels each time
+    func updateUI() {
+        numberLabel.text = String(newGame?.getNewNumber() ?? 0)
+        scoreLabel.text = "Score: " + String(newGame?.getScore() ?? 0)
+    }
+    
+    // If "higher" is pressed
+    @IBAction func higherButtonPressed(_ sender: UIButton) {
+        // Check if answer is correct
+        if(newGame!.checkNumbers(userPressedHigher: true)) {
+            // Update score is answer is correct
+            newGame?.score += 1
+        }
+        updateUI()
+    }
+    
+    // If lower is pressed
+    @IBAction func lowerButtonPressed(_ sender: UIButton) {
+        // Check if answer is correct
+        if(newGame!.checkNumbers(userPressedHigher: false)){
+            // Update score is answer is correct
+            newGame?.score += 1
+        }
+        updateUI()
+    }
+}
